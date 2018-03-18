7a0b9221ff62ad921164665a31704b5c800d1165
Mallika Aruva <macr7@mst.edu>
2018-03-18 18:15:10 -0500

    Add files via upload
    Pixel art  Maker

---
+++ control.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // message for device width
+  $('.maxWidth').text('The maximum value for your device\'s width are: ' + controlDeviceWidth() + ' columns.');
+  // submit button
+  $('#btn-submit').click(makeGrid);
+  // add color
+  $('#grid').click(function(event) {
+    addColor($(event.target));
+  });
+});
